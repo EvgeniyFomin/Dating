db2d4adf203b4e2470c5dc1c8fc9e93333fc36cd
Evgeniy Fomin <evgeniusf@gmail.com>
2024-09-16 12:40:09 +0300

    Persisting user functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,25 @@ import { NavComponent } from "./nav/nav.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'Dating Client';
+  private accountService = inject(AccountService);
   httpClient = inject(HttpClient);
+  title = 'Dating Client';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.httpClient.get('https://localhost:5001/users').subscribe({
       next: users => this.users = users,
       error: error => console.log(error),
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -10,22 +10,22 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
+
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
+  accountService = inject(AccountService);
+
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   };
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
