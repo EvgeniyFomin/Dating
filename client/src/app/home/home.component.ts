67eae5498e442d6caa1b3451c7bdc559d1b0f315
Evgeniy Fomin <evgeniusf@gmail.com>
2024-09-16 14:01:16 +0300

    cleanup - Renaming the variable

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  isRegistered = false;
+  isRegisterMode = false;
 
   registerToggle() {
-    this.isRegistered = !this.isRegistered;
+    this.isRegisterMode = !this.isRegisterMode;
   }
 }
