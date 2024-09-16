0056288078720cf3e84d5357063098bc9726a923
Evgeniy Fomin <evgeniusf@gmail.com>
2024-09-16 16:09:22 +0300

    Parent to child communication Added signal input to register component to transfer the data from home to register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -14,12 +13,8 @@ import { HomeComponent } from "./home/home.component";
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
-  httpClient = inject(HttpClient);
-  title = 'Dating Client';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -29,13 +24,5 @@ export class AppComponent implements OnInit {
 
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.httpClient.get('https://localhost:5001/users').subscribe({
-      next: users => this.users = users,
-      error: error => console.log(error),
-      complete: () => console.log('users got successfully')
-    })
   }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,23 @@ import { RegisterComponent } from "../register/register.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  httpClient = inject(HttpClient);
   isRegisterMode = false;
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
   registerToggle() {
     this.isRegisterMode = !this.isRegisterMode;
+  }
+  getUsers() {
+    this.httpClient.get('https://localhost:5001/users').subscribe({
+      next: users => this.users = users,
+      error: error => console.log(error),
+      complete: () => console.log('users got successfully')
+    })
   }
 }
