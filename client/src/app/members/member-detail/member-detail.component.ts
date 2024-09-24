fca8d61e2cef86cdaae19dc4c4d5f0de5db8d82f
Evgeniy Fomin <evgeniusf@gmail.com>
2024-09-25 00:13:21 +0300

    Added gallery Addd default photo for the user without photo

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,11 +3,12 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -17,6 +18,7 @@ export class MemberDetailComponent implements OnInit {
   private routes = inject(ActivatedRoute);
 
   member?: Member;
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
     this.loadMember();
@@ -27,10 +29,17 @@ export class MemberDetailComponent implements OnInit {
     if (id) {
       this.memberService.getMemberById(id).subscribe(
         {
-          next: member => this.member = member
+          next: member => {
+            this.member = member;
+            member.photos.map(p => {
+              this.images.push(new ImageItem({
+                src: p.url,
+                thumb: p.url
+              }))
+            })
+          }
         }
       );
     }
-
   }
 }
