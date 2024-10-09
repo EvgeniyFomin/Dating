803c1c7462293d5f9d49344a15aacc5a2c6d1414
Evgeniy Fomin <evgeniusf@gmail.com>
2024-10-09 14:58:48 +0300

    Added paging models Added pagination extension

---
+++ Dating.Core/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Dating.Core.Models.Pagination;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Dating.Core.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader<T>(this HttpResponse response, PagedList<T> data)
+        {
+            var paginationHeader = new PaginationHeader(data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
+
+            var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            response.Headers.Append("Pagination", JsonSerializer.Serialize(paginationHeader, jsonOptions));
+            response.Headers.Append("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
