1d94a45aae0d8c3132fe7237469ee63f239645ae
Evgeniy Fomin <evgeniusf@gmail.com>
2024-10-24 20:04:41 +0300

    Added message model and dto to core Added enum for messaging

---
+++ Dating.Core/Dtos/CreateMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Dating.Core.Dtos
+{
+    public class CreateMessageDto
+    {
+        public int RecipientId { get; set; }
+        public required string Content { get; set; }
+    }
+}
---
+++ Dating.Core/Dtos/MessageUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Dating.Core.Dtos
+{
+    public class MessageUserDto
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string PhotoUrl { get; set; }
+    }
+}
