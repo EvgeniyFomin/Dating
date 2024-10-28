9dde3d137c324c17cef4439a8aa5e7d3154106a4
Evgeniy Fomin <evgeniusf@gmail.com>
2024-10-28 13:09:06 +0200

    added member message component

---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,28 @@
+import { TimeagoModule } from 'ngx-timeago';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { MessagesService } from '../../_services/messages.service';
+import { Message } from '../../_models/message';
+
+@Component({
+  selector: 'app-member-messages',
+  standalone: true,
+  imports: [TimeagoModule],
+  templateUrl: './member-messages.component.html',
+  styleUrl: './member-messages.component.css'
+})
+
+export class MemberMessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
+  userId = input.required<number>();
+  messages: Message[] = [];
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messagesService.getThread(this.userId()).subscribe({
+      next: messages => this.messages = messages
+    })
+  }
+}
