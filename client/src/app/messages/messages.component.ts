0aea1ad1b27aab3430ba70be2fac7703896f4b80
Evgeniy Fomin <evgeniusf@gmail.com>
2024-10-28 11:52:56 +0200

    updated message component added markup for the incoming and outgoing messages

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../_services/messages.service';
+import { Message } from '../_models/message';
+import { MessageParams } from '../_models/messageParams';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { RouterLink } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterLink, PaginationModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
+  thread: Message[] = [];
+  parameters: MessageParams = new MessageParams('1');
 
+  ngOnInit(): void {
+    this.loadMessages();
+
+    this.messagesService.getThread(4).subscribe({
+      next: messages => this.thread = messages
+    });
+  }
+
+  loadMessages() {
+    this.messagesService.getMessages(this.parameters);
+  }
+
+  pageChanged(event: any) {
+    if (this.parameters.pageNumber !== event.page) {
+      this.parameters.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
+
+  getRoute(message: Message) {
+    if (this.parameters.container === '2') return `/members/${message.recipient.id}`
+    else return `/members/${message.sender.id}`
+  }
 }
