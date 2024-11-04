664accf5f12a0a5de3ef567c03ad3acd6a8a0312
Evgeniy Fomin <evgeniusf@gmail.com>
2024-11-04 19:42:25 +0200

    Added signalr to client

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes.service';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ import { LikesService } from './likes.service';
 export class AccountService {
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   private baseUrl = environment.apiUrl + 'accounts/';
   currentUser = signal<User | null>(null);
   roles = computed(() => {
@@ -36,6 +38,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUser.set(null);
+    this.presenceService.stopHubConnection();
   }
 
   register(model: any) {
@@ -53,6 +56,6 @@ export class AccountService {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
     this.likesService.getLikeIds();
-
+    this.presenceService.createHubConnection(user);
   }
 }
