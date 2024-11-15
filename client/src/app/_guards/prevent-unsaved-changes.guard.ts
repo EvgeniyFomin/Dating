ac0314273c859cbfea3f62d4f3f297b5de9ddb69
Evgeniy Fomin <evgeniusf@gmail.com>
2024-11-15 14:33:49 +0200

    added confirm dialog added confirmation service updated guard to use confirmation dialog

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,9 +1,12 @@
 import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { inject } from '@angular/core';
+import { ConfirmService } from '../_services/confirm.service';
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmServie = inject(ConfirmService)
   if (component.editForm?.dirty) {
-    return confirm('Are you sure you want to continue? any unsaved changes will be lost');
+    return confirmServie.confirm() ?? false;
   }
 
   return true;
