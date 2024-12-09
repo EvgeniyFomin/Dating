feb565942fca9d5859968812038e60f77ad2741f
Evgeniy Fomin <evgeniusf@gmail.com>
2024-12-10 00:31:42 +0200

    Migrations added for scratch DB

--- Dating.API/Extensions/DataContextServiceExtensions.cs
+++ Dating.API/Extensions/DataContextServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Dating.Core.Models;
 using Dating.DAL.Context;
+using Dating.DAL.Repositories.Interfaces;
+using Dating.DAL.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Role = Dating.Core.Models.Identity.Role;
@@ -10,6 +12,13 @@ namespace Dating.API.Extensions
     {
         public static IServiceCollection AddDataContextServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessagesRepository, MessagesRepository>();
+            services.AddScoped<IGroupsRepository, GroupsRepository>();
+            services.AddScoped<IPhotosRepository, PhotosRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddIdentityCore<User>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
@@ -20,8 +29,8 @@ namespace Dating.API.Extensions
 
             services.AddDbContext<DataContext>(opt =>
             {
-                opt.UseSqlite(
-                    configuration.GetConnectionString("SQLite"),
+                opt.UseSqlServer(
+                    configuration.GetConnectionString("SqlConnection"),
                     options => options.MigrationsAssembly("Dating.DAL"));
             });
 
